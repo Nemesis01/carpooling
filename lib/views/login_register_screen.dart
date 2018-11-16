0706da993f03be31efc41c62d0d3894ac87fb796
Stéphane Nton <stephane.nton@gmail.com>
2018-11-16 16:24:50 -0500

    Adding login_register_screen view

---
+++ lib/views/login_register_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterScreen extends StatefulWidget {
+  @override
+  _LoginRegisterScreen createState() => _LoginRegisterScreen();
+}
+
+class _LoginRegisterScreen extends State<LoginRegisterScreen> {
+  ///
+  ///
+  ///
+  ///
+  @override
+  void initState() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+  @override
+  void dispose() {}
+
+  ///
+  ///
+  ///
+  ///
+
+}
